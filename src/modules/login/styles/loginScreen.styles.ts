23e76e55c9ac875ed6c8271e3a850e513d016d07
Giulia <giulia.saunders@alu.ufc.br>
2024-10-15 16:11:04 -0300

    feat: create login component

--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
 
+export const ContainerLoginScreen = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: right;
+`;
+
 export const BackgroundImage = styled.img`
     position: absolute;
     left: 0;
     top: 0;
     width: 100%;   // % indica a largura máxima do pai (body)
-    height: 100vh; // vh: altura vertical, 100 é o máximo, não usa %, pois o body não possui altura máxima
-    object-fit: cover; // ajustar a imagem ao espaço (body) sem alterar suas proporções
+    height: 100vh; // vh: altura vertical, 100 é o máximo, não usa %, pois o body não possui altura máxima da tela inteira
+    object-fit: cover; // ajustar a imagem ao espaço sem alterar suas proporções
+    z-index: -1;
+`;
+
+export const LogoImage = styled.img``;
+
+export const ContainerLogin = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #D9D9D9;
+    padding: 22px;
+    width: 100%;
+    height: 100vh;
+    max-width: 646px;
+    z-index: 2;
+`;
+
+export const LimitedContainer = styled.div`
+    width: 100%;
+    max-width: 498px;
+    background-color: blue;
+    display: flex;
+    justify-content: center;
 `;
 
